5bc4ea425ee00793a5a43d52b137c14d4dd9c6d6
frank white <amywinehousing@protonmail.com>
2026-01-11 15:03:39 +0200

    Modify install.sh to move and set permissions for files
    Updated installation script to include permissions and additional files.

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 pacman -S nodejs npm
-npm install electron
 mkdir /usr/bin/flirbnet
 mkdir /usr/bin/flirbnet/assets
 mv package.json /usr/bin/flirbnet
 mv package-lock.json /usr/bin/flirbnet
 mv preload.js /usr/bin/flirbnet
 mv main.js /usr/bin/flirbnet
-mv launch.sh /usr/bin/flirbnet
-mv node_modules /usr/bin/flirbnet
 mv assets/icon.png /usr/bin/flirbnet/assets
 mv assets/flirb.desktop /usr/share/applications
+chmod +x launch.sh
+chmod +x install2.sh
+mv launch.sh /usr/bin/flirbnet
+mv install2.sh /usr/bin/flirbnet
+./install2.sh
