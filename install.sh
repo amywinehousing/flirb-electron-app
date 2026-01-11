6d5d1f72cb16d5eb53446cd14f916469c0d581fd
frank white <amywinehousing@protonmail.com>
2026-01-11 12:35:06 +0000

    fix

--- install.sh
+++ install.sh
@@ -2,10 +2,12 @@
 pacman -S nodejs npm
 npm install electron
 mkdir /usr/bin/flirbnet
+mkdir /usr/bin/flirbnet/assets
 mv package.json /usr/bin/flirbnet
 mv package-lock.json /usr/bin/flirbnet
 mv preload.js /usr/bin/flirbnet
 mv main.js /usr/bin/flirbnet
 mv launch.sh /usr/bin/flirbnet
 mv node_modules /usr/bin/flirbnet
-mv assets /usr/bin/flirbnet
+mv assets/icon.png /usr/bin/flirbnet/assets
+mv assets/flirb.desktop /usr/share/applications
