9bad544a4adc7a7cdfc2a49db6be21f10fd767ac
frank white <amywinehousing@protonmail.com>
2026-01-11 01:59:19 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      enableRemoteModule: false,
+      webSecurity: true
+    },
+    icon: path.join(__dirname, 'icon.png')
+  });
+
+  mainWindow.loadURL('https://flirb.net');
+  mainWindow.setMenu(null);
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
